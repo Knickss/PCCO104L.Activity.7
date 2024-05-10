c2b3643e004242fa549cf364b6a8a043d8a057b8
nicco <nicco@LAPTOP-GOVEPLR0>
2024-05-10 14:59:24 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character ArthurMorgan = new Character(15000, "Arthur Morgan", "Double-Action Revolver");
+            ArthurMorgan.Intro();
+
+            Character JohnMarston = new Character(1000, "John Marston", "Cattleman Revolver");
+            JohnMarston.Intro();
+
+            Character DutchVanDerLinde = new Character(2000, "Dutch van der Linde", "Semi-automatic Pistol");
+            DutchVanDerLinde.Intro();
+        }
+    }
+
+    public class Character
+    {
+        private string Name { get; set; }
+
+        private string Weapon { get; set; }
+
+        private int Bounty { get; set; }
+
+        public Character() { }
+
+        public Character(int bounty)
+        {
+            Bounty = bounty;
+        }
+
+        public Character(int bounty, string name) : this(bounty)
+        {
+            Name = name;
+        }
+
+        public Character(int bounty, string name, string weapon) : this(bounty, name)
+        {
+            Weapon = weapon;
+        }
+
+        public void Intro()
+        {
+            Console.WriteLine($"{Name} - {Weapon} - Bounty: ${Bounty}");
+        }
+    }
+}
